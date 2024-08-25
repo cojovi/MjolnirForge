29bc394e89bd4d9f29383bd41df768b0a072359a
cojovi <31863818+cojovi@users.noreply.github.com>
2024-08-25 05:56:05 -0500

    yrd
    yrd

--- theforge/app/src/landing-page/components/Footer.tsx
+++ theforge/app/src/landing-page/components/Footer.tsx
@@ -20,7 +20,7 @@ export default function Footer({ footerNavigation }: {
         </h2>
         <div className='flex items-start justify-end mt-10 gap-20'>
           <div>
-            <h3 className='text-sm font-semibold leading-6 text-gray-900 dark:text-white'>Titties-App</h3>
+            <h3 className='text-sm font-semibold leading-6 text-gray-900 dark:text-white'>CMAC Forge</h3>
             <ul role='list' className='mt-6 space-y-4'>
               {footerNavigation.app.map((item) => (
                 <li key={item.name}>
